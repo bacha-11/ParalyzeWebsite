96eafaa5ef6e91e9db2c424ceab285601772a658
bacha-11 <ijazbacha14@gmail.com>
2021-04-26 21:45:34 +0500

    create a static folder inside in app

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,4 @@ from flask import redirect, render_template, url_for
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Home Page')
