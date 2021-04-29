cd0d63d5a4f63d22e9de21ce4dc9c2fbfa8d1599
bacha-11 <ijazbacha14@gmail.com>
2021-04-29 23:06:58 +0500

    design a responsive home page

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,8 +1,20 @@
+
+// provide color to the active link
 let currentLocation = location.href
 let addActive = document.querySelectorAll('.addActive');
-for(let i=0; i<addActive.length; i++ ){
+for(let i=0; i<addActive.length; i++ ) {
 
-    if (addActive[i].href === currentLocation){
-        addActive[i].classList.add('active')
+    if (addActive[i].href === currentLocation) {
+        addActive[i].classList.add('active');
     }
 }
+
+
+// Change navbar logo
+// let checkBtn = document.querySelector('.checkBtn');
+
+// function changeLoge(){
+//     checkBtn.innerHTML = '<i class="fas fa-remove"></i>'
+// }
+
+// checkBtn.addEventListener('click', changeLoge)
